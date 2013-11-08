8111cf94a0931bf95f32d01527836595270aed1a
Michael Huang <hyc1124@gmail.com>
2013-11-09 00:30:39 +0800

    validation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,32 @@ class PostsController < ApplicationController
   def show
   	@post = Post.find(params[:id])
   end
+
+  def new
+  	@post = Post.new
+  end
+
+  def create
+  	@post = Post.new(post_params)
+
+  	if @post.save
+  		flash[:notice] = 'Your post was created.'
+  		redirect_to posts_path
+  	else # validation error, we can display error messages
+  		render 'new' # which you submitted this action from
+  	end
+  end
+
+	def edit
+  	
+  end
+
+  def update
+
+  end
+
+	def post_params
+		params.require(:post).permit!
+	end
+
 end
